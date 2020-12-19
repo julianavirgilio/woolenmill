a3b913312383cd29a10d9b835ec467020e90a870
Amy <74540344+13-8billion@users.noreply.github.com>
2020-12-19 10:53:56 +0000

    ..

--- js/mainProject.js
+++ js/mainProject.js
@@ -134,8 +134,8 @@ function validateFormLogin()
 function myFunction() {
   // Declare variables
   var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
+  input = document.getElementById('myInput'); // take input from the user
+  filter = input.value.toUpperCase(); // make all input uppercase
   ul = document.getElementById("productList");
   li = ul.getElementsByTagName('li');
 
@@ -143,10 +143,10 @@ function myFunction() {
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("a")[0];
     txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    if (txtValue.toUpperCase().indexOf(filter) > -1) { // if input value equals product display
       li[i].style.display = "";
     } else {
-      li[i].style.display = "none";
+      li[i].style.display = "none"; //else hide product
     }
   }
 }
